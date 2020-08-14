16849a968d704e38963f5f2dd53a5763cd0fd7e4
Yury Bikuzin <yury.bikuzin@gmail.com>
2020-08-14 18:29:57 +0300

    added provide util: src/ and data/normalized|caterory|also_view{_used}.csv.zip

---
+++ data/README.md
@@ -0,0 +1,46 @@
+# Файлы с данными
+
+
+<!-- vim-markdown-toc Redcarpet -->
+
+* [Трейновый и тестовый датасеты](#трейновый-и-тестовый-датасеты)
+* [Sample submission](#sample-submission)
+* [Json-файл с дополнительными данными](#json-файл-с-дополнительными-данными)
+* [meta_Grocery_and_Gourmet_Food.json в виде csv-файла](#meta_grocery_and_gourmet_food-json-в-виде-csv-файла)
+* [Нормализованные данные из meta_Grocery_and_Gourmet_Food.json](#нормализованные-данные-из-meta_grocery_and_gourmet_food-json)
+* [Нормализованные данные из meta_Grocery_and_Gourmet_Food.json, но только для тех item'ов, которые используются либо в трейновом, либо в тестовом датасетах](#нормализованные-данные-из-meta_grocery_and_gourmet_food-json-но-только-для-тех-item-39-ов-которые-используются-либо-в-трейновом-либо-в-тестовом-датасетах)
+
+<!-- vim-markdown-toc -->
+
+## Трейновый и тестовый датасеты
+
+- train.csv.zip
+- test.csv.zip
+
+## Sample submission
+
+- sample_submission.csv.zip
+
+## Json-файл с дополнительными данными
+
+- meta_Grocery_and_Gourmet_Food.json.zip
+
+## meta_Grocery_and_Gourmet_Food.json в виде csv-файла
+
+- json.csv.zip
+
+## Нормализованные данные из meta_Grocery_and_Gourmet_Food.json
+
+- normalized.csv.zip
+- category.csv.zip
+- also_view.csv.zip
+
+## Нормализованные данные из meta_Grocery_and_Gourmet_Food.json, но только для тех item'ов, которые используются либо в трейновом, либо в тестовом датасетах
+
+- normalized_used.csv.zip
+- category_used.csv.zip
+- also_view_used.csv.zip
+
+
+
+
--- src/provide/src/main.rs
+++ src/provide/src/main.rs
@@ -1,15 +1,25 @@
 // fn main() {
 //     println!("Hello, world!");
 // }
+// #![recursion_limit="4096"]
 #[allow(unused_imports)]
 use log::{error, warn, info, debug, trace};
 #[allow(unused_imports)]
-use anyhow::{Result, Error, bail, anyhow};
+use anyhow::{Result, Error, bail, anyhow, Context};
 
+use structopt::StructOpt;
+use std::path::PathBuf;
 // use std::fs;
 use std::fs::File;
 use std::io::BufReader;
-// use std::io::prelude::*;
+use std::io::prelude::*;
+//
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// Path to toml config file
+    #[structopt(parse(from_os_str), default_value = "../data")]
+    data: PathBuf,
+}
 
 use serde::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize)]
@@ -25,11 +35,11 @@ pub struct Item {
     // pub rank: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Simple {
     pub also_view: Option<String>,
     pub asin: String,
-    // pub itemid: Option<u64>,
+    pub itemid: Option<usize>,
     pub brand: Option<String>,
     pub category: String,
     pub description: Option<String>,
@@ -38,10 +48,10 @@ pub struct Simple {
     pub price: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Normalized {
     pub asin: String,
-    // pub itemid: Option<u64>,
+    pub itemid: Option<usize>,
     pub brand: Option<String>,
     pub description: Option<String>,
     pub title: Option<String>,
@@ -49,40 +59,113 @@ pub struct Normalized {
     pub price: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NormalizedUsed {
+    pub itemid: usize,
+    pub brand: Option<String>,
+    pub description: Option<String>,
+    pub title: Option<String>,
+    pub main_cat: Option<String>,
+    pub price: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Category {
     pub asin: String,
-    // pub itemid: Option<u64>,
+    pub itemid: Option<usize>,
     pub category: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CategoryUsed {
+    pub itemid: usize,
+    pub category: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AlsoViewUsed {
+    pub itemid: usize,
+    pub also_view_itemid: usize,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct AlsoView {
     pub asin: String,
-    // pub itemid: Option<u64>,
+    pub itemid: Option<usize>,
     pub also_view: String,
+    pub also_view_itemid: Option<usize>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Record {
-    pub overall: String,
-    pub verified: String,
-    pub review_time: String,
+    pub itemid: usize,
     pub asin: String,
-    pub reviewer_name: String,
-    pub review_text: String,
-    pub summary: String,
-    pub unix_review_time: String,
-    pub vote: Option<usize>,
-    pub style: Option<String>,
-    pub image: Option<String>,
-    pub userid: u64,
-    pub itemid: u64,
-    pub rating: f32,
 }
 
-// use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+
+use std::sync::{Arc, RwLock};
+
+#[derive(Clone)]
+pub struct VecMut {
+    vec: Arc<RwLock<Vec<u8>>>,
+}
+
+impl VecMut {
+    pub fn new() -> Self {
+        Self {
+            vec: Arc::new(RwLock::new(Vec::new()))
+        }
+    }
+    pub fn len(&self) -> usize {
+        self.vec.read().unwrap().len()
+    }
+}
+
+impl std::io::Write for VecMut {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let mut vec = self.vec.write().expect("RwLock::write()");
+        for i in buf {
+            (*vec).push(*i)
+        }
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
+const COUNT_SIMPLE: usize = 287208;
+const COUNT_CATEGORY: usize = 1120436;
+const COUNT_ALSO_VIEW: usize = 1899302;
+const COUNT_NORMALIZED: usize = COUNT_SIMPLE;
+
+macro_rules! save_csv {
+    ($data_dir: expr, $var: ident) => {
+        let filename = &stringify!($var)[4..];
+        save_csv!($data_dir, $var, filename);
+    };
+    ($data_dir: expr, $var: ident, $filename: expr) => {
+        let filename =format!("{}.csv", $filename);
+        let buf = VecMut::new();
+        let mut wtr = csv::Writer::from_writer(buf.clone());
+        for record in $var {
+            wtr.serialize(record)?;
+        }
+        wtr.flush()?;
+
+        let mut filepath = $data_dir.clone();
+        filepath.push(format!("{}.zip", &filename));
+        let file = std::fs::File::create(&filepath).unwrap();
+        let mut zip = zip::ZipWriter::new(file);
+        let options = zip::write::FileOptions::default()
+            .compression_method(zip::CompressionMethod::Deflated)
+            .unix_permissions(0o755);
+        zip.start_file(filename, options)?;
+        zip.write_all(&buf.vec.read().unwrap())?;
+        zip.finish()?;
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -92,37 +175,51 @@ async fn main() -> Result<()> {
         std::env::set_var("RUST_LOG", "warn");
     }
 
-    // let filepath = "/data/train.csv";
-    // let file = File::open(filepath)
-    //     .expect("could not open file");
-    // let buffered_reader = BufReader::new(file);
-    // let mut rdr = csv::Reader::from_reader(buffered_reader);
-    // let mut asin2itemid: HashMap<String, u64> = HashMap::new();
-    // for result in rdr.deserialize() {
-    //     // Notice that we need to provide a type hint for automatic
-    //     // deserialization.
-    //     let record: Record = result?;
-    //     asin2itemid.insert(record.asin, record.itemid);
-    // }
-    // println!("asin2itemid: {}", asin2itemid.len());
-    // return Ok(());
-    // let deserializer = serde_json::Deserializer::from_reader(buffered_reader);
+    let opt = Opt::from_args();
+    let data_dir = PathBuf::from(&opt.data);
 
-    let filepath = "/data/meta_Grocery_and_Gourmet_Food.json";
-    let file = File::open(filepath)
+    let mut filepath = data_dir.clone();
+    filepath.push("itemid_asin.csv");
+    let file = File::open(&filepath)
         .expect("could not open file");
     let buffered_reader = BufReader::new(file);
+    let mut rdr = csv::Reader::from_reader(buffered_reader);
+    let mut asin2itemid: HashMap<String, usize> = HashMap::new();
+    let mut set_itemid: HashSet<usize> = HashSet::new();
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        asin2itemid.insert(record.asin, record.itemid);
+        set_itemid.insert(record.itemid);
+    }
+    println!("asin2itemid: {}, set_itemid: {}", asin2itemid.len(), set_itemid.len());
+
+    let start = std::time::Instant::now();
+    let mut filepath = data_dir.clone();
+    filepath.push("meta_Grocery_and_Gourmet_Food.json.zip");
+    let file = File::open(&filepath).context(format!("{:?}", filepath))?;
+    let buffered_reader = BufReader::new(file);
+    let mut zip = zip::ZipArchive::new(buffered_reader)?;
+    let mut file = zip.by_index(0).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    println!("unzip: {}", arrange_millis::get(std::time::Instant::now().duration_since(start).as_millis()));
+    let buffered_reader = BufReader::new(contents.as_bytes());
+    
     let deserializer = serde_json::Deserializer::from_reader(buffered_reader);
     
     let iterator = deserializer.into_iter::<Item>();
     let start = std::time::Instant::now();
-    let mut vec_simple: Vec<Simple> = vec![];
-    let mut vec_normalized: Vec<Normalized> = vec![];
-    let mut vec_category: Vec<Category> = vec![];
-    let mut vec_also_view: Vec<AlsoView> = vec![];
+    let mut vec_simple: Vec<Simple> = Vec::with_capacity(COUNT_SIMPLE);
+    let mut vec_normalized: Vec<Normalized> = Vec::with_capacity(COUNT_NORMALIZED);
+    let mut vec_category: Vec<Category> = Vec::with_capacity(COUNT_CATEGORY);
+    let mut vec_also_view: Vec<AlsoView> = Vec::with_capacity(COUNT_ALSO_VIEW);
+
+    let mut vec_normalized_used: Vec<NormalizedUsed> = Vec::with_capacity(COUNT_NORMALIZED);
+    let mut vec_category_used: Vec<CategoryUsed> = Vec::with_capacity(COUNT_CATEGORY);
+    let mut vec_also_view_used: Vec<AlsoViewUsed> = Vec::with_capacity(COUNT_ALSO_VIEW);
 
     use indicatif::{ProgressBar, ProgressStyle};
-    let pbar = ProgressBar::new(300000);
+    let pbar = ProgressBar::new(COUNT_SIMPLE as u64);
     pbar.set_style(
         ProgressStyle::default_bar()
             .template("{eta_precise} [{bar:60.cyan/blue}] {wide_msg}")
@@ -137,10 +234,10 @@ async fn main() -> Result<()> {
             None => None,
             Some(vec_string) => Some(vec_string.join("\n\n")),
         };
-        // let itemid = asin2itemid.get(&item.asin);
+        let itemid = asin2itemid.get(&item.asin);
         let normalized = Normalized {
             asin: item.asin.clone(),
-            // itemid: itemid.copied(),
+            itemid: itemid.copied(),
             brand: item.brand.clone(),
             description: description.clone(),
             title: item.title.clone(),
@@ -150,17 +247,45 @@ async fn main() -> Result<()> {
         for category in &item.category {
             vec_category.push(Category {
                 asin: item.asin.clone(),
-                // itemid: itemid.copied(),
+                itemid: itemid.copied(),
                 category: category.clone(),
             });
         }
         if let Some(vec_string) = &item.also_view {
             for s in vec_string {
+                let also_view_itemid = asin2itemid.get(s.as_str());
                 vec_also_view.push(AlsoView {
                     asin: item.asin.clone(),
-                    // itemid: itemid.copied(),
+                    itemid: itemid.copied(),
                     also_view: s.clone(),
+                    also_view_itemid: also_view_itemid.copied(),
                 });
+            }
+        }
+        if let Some(itemid) = itemid {
+            vec_normalized_used.push(NormalizedUsed {
+                itemid: *itemid,
+                brand: item.brand.clone(),
+                description: description.clone(),
+                title: item.title.clone(),
+                main_cat: item.main_cat.clone(),
+                price: item.price.clone(),
+            });
+            for category in &item.category {
+                vec_category_used.push(CategoryUsed {
+                    itemid: *itemid,
+                    category: category.clone(),
+                });
+            }
+            if let Some(vec_string) = &item.also_view {
+                for s in vec_string {
+                    if let Some(also_view_itemid) = asin2itemid.get(s.as_str()) {
+                        vec_also_view_used.push(AlsoViewUsed {
+                            itemid: *itemid,
+                            also_view_itemid: *also_view_itemid,
+                        });
+                    }
+                }
             }
         }
         let simple = Simple {
@@ -169,7 +294,7 @@ async fn main() -> Result<()> {
                 Some(vec_string) => Some(vec_string.join("|")),
             },
             asin: item.asin.clone(),
-            // itemid: itemid.copied(),
+            itemid: itemid.copied(),
             brand: item.brand.clone(),
             category: item.category.join("|"),
             description: description.clone(),
@@ -178,42 +303,33 @@ async fn main() -> Result<()> {
             price: item.price.clone(),
         };
         vec_simple.push(simple);
-
         vec_normalized.push(normalized);
         pbar.set_position(i as u64);
     }
     pbar.finish();
 
-    println!("{}, simple: {}, normalized: {}, category: {}, also_view: {}", arrange_millis::get(std::time::Instant::now().duration_since(start).as_millis()), vec_simple.len(), vec_normalized.len(), vec_category.len(), vec_also_view.len());
-
+    println!("{}, simple: {}, normalized/used: {}/{}, category/used: {}/{}, also_view/used: {}/{}", 
+        arrange_millis::get(std::time::Instant::now().duration_since(start).as_millis()), 
+        vec_simple.len(), 
+        vec_normalized.len(), 
+        vec_normalized_used.len(), 
+        vec_category.len(), 
+        vec_category_used.len(), 
+        vec_also_view.len(),
+        vec_also_view_used.len(),
+    );
 
     let start = std::time::Instant::now();
 
-    let mut wtr = csv::Writer::from_path("/data/json.csv")?;
-    for record in vec_simple {
-        wtr.serialize(record)?;
-    }
-    wtr.flush()?;
+    save_csv!(data_dir, vec_simple, "json");
+    save_csv!(data_dir, vec_normalized);
+    save_csv!(data_dir, vec_category);
+    save_csv!(data_dir, vec_also_view);
+    save_csv!(data_dir, vec_normalized_used);
+    save_csv!(data_dir, vec_category_used);
+    save_csv!(data_dir, vec_also_view_used);
 
-    let mut wtr = csv::Writer::from_path("/data/normalized.csv")?;
-    for record in vec_normalized {
-        wtr.serialize(record)?;
-    }
-    wtr.flush()?;
-
-    let mut wtr = csv::Writer::from_path("/data/category.csv")?;
-    for record in vec_category {
-        wtr.serialize(record)?;
-    }
-    wtr.flush()?;
-
-    let mut wtr = csv::Writer::from_path("/data/also_view.csv")?;
-    for record in vec_also_view {
-        wtr.serialize(record)?;
-    }
-    wtr.flush()?;
-
-    println!("{}", arrange_millis::get(std::time::Instant::now().duration_since(start).as_millis()));
+    println!("write *.csv.zip: {}", arrange_millis::get(std::time::Instant::now().duration_since(start).as_millis()));
 
     Ok(())
 }
